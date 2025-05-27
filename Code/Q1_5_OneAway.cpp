0e27a1891dec7fb043554ce4091faa24523969d5
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-05-27 23:03:02 +0530

    today iough.cpp

---
+++ Code/Q1_5_OneAway.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a real string : ";
+    getline(cin, str); // string input leva
+    string str2;
+    cout << "Enter another string for compare : ";
+    getline(cin, str2);
+
+    if(str2.length() == str1.length()-1 || str2.length() == str1.length() + 1)
+
+
+    return 0;
+}
