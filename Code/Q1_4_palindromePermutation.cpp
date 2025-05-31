fe5f305a080e34d9a1f76b678fbd7feedd87e92c
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-05-31 22:06:15 +0530

    twoo question half solved.cpp

---
+++ Code/Q1_4_palindromePermutation.cpp
@@ -0,0 +1,18 @@
+// incomplete.
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void palindromePermutation(string str){
+    if 
+}
+int main(){
+
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    palindromePermutation(str);
+    return 0;
+}
--- Code/Q1_5_OneAway.cpp
+++ Code/Q1_5_OneAway.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
+
+void stringAway(string str, string str2){
+
+    if (str.find(str2) != string::npos || str2.find(str) != string::npos) {
+        cout << "The strings are one away from each other." << endl;
+    } else {
+        cout << "The strings are not one away from each other." << endl;
+    }
+}
 
 int main(){
     string str;
@@ -9,9 +18,17 @@ int main(){
     string str2;
     cout << "Enter another string for compare : ";
     getline(cin, str2);
-
-    if(str2.length() == str1.length()-1 || str2.length() == str1.length() + 1)
-
-
+    
+    
+    stringAway(str, str2);
+    
     return 0;
 }
+    /*
+    
+    str.find(substr) returns the index of the first occurrence 
+    of substr in str, or string::npos if substr is not found.
+    
+    So, str1.find(str2) != string::npos checks if str2 is a substring of str1, and vice versa.
+    
+    */
