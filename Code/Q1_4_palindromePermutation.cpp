0d009460a52be9a1a6f3b5bffce54b945a3a8b4d
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-06-14 18:00:45 +0530

    palindrome permutation completed

--- Code/Q1_4_palindromePermutation.cpp
+++ Code/Q1_4_palindromePermutation.cpp
@@ -4,15 +4,34 @@
 #include<string.h>
 using namespace std;
 
-void palindromePermutation(string str){
-    if 
+bool ispalindromePermutation(string str1){
+    int charCount[256] = {0}; // Assuming ASCII characters
+    int oddCount = 0;
+
+    for (char c : str1){
+        if(c != ' '){
+            charCount[tolower(c)]++; // Convert to lowercase and count characters
+        }
+    }
+    for (int i = 0 ; i < 256; i++){
+        if (charCount[i] % 2 != 0){
+            oddCount++;
+        }
+    }
+    return oddCount <= 1; // At most one odd count is allowed
 }
+
 int main(){
 
-    string str;
-    cout << "Enter a string: ";
-    getline(cin, str);
+    string str1;
 
-    palindromePermutation(str);
+    cout << "Enter a string: ";
+    getline(cin, str1);
+
+    if (ispalindromePermutation(str1)){
+        cout << "The string is a permutation of a palindrome." << endl;
+    } else {
+        cout << "The string is not a permutation of a palindrome." << endl;
+    }
     return 0;
 }
