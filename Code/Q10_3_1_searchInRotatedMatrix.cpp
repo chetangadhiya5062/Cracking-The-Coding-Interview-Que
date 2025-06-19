89b274ed9efadb9a2b4f7adbe6db2e6dfa7d3c69
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-06-19 23:41:32 +0530

    searchRotatedArray and Matrix, incomplete.cpp

--- Code/Q10_3_1_searchInRotatedMatrix.cpp
+++ Code/Q10_3_1_searchInRotatedMatrix.cpp
@@ -1,12 +1,3 @@
-/*
-10.3 Search in Rotated Array: Given a sorted array of n integers that has been rotated an unknown
-number of times, write code to find an element in the array. You may assume that the array was
-originally sorted in increasing order.
-EXAMPLE
-lnput:findSin{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
-Output: 8 (the index of 5 in the array)
-*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>// for swap
