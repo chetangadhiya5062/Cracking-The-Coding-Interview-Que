95174d56d86082824ea0653a56d554aa5f20051b
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-06-24 20:54:56 +0530

    Q10_10_rankFromStream.cpp

--- Code/Q10_10_rankFromStream.cpp
+++ Code/Q10_10_rankFromStream.cpp
@@ -10,3 +10,46 @@ getRankOfNumber(l) 0
 getRankOfNumber(3) = 1
 getRankOfNumber(4) 3
 */
+
+#include <iostream>
+// #include <vector>
+
+using namespace std;
+
+void printArray(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int rankFromStream(int arr[], int size, int n){
+    // vector<int> vectArray;
+    int count = 0;
+
+    for (int i = 0 ; i < size ; i++){
+        if (arr[i] < n){
+            // vectArray.push_back(arr[i]);
+            // or
+            count ++;
+        }
+    }
+    // return vectArray.size();
+    //or
+    return count;
+}
+int main(){
+    int arr[] = {5, 1, 4, 4, 5, 9, 7, 13, 3};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int n;
+
+    cout << "Array is : " ;
+    printArray(arr, size);
+
+    cout << "Find the rank of n = ";
+    cin >> n;
+
+    int rank = rankFromStream(arr, size, n);
+    cout << "rank of " << n << " is " << rank;
+    return 0;
+}
